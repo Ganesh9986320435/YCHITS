7bd6788ae484b6362c1d77fd00a948b823ce3ab6
Yerradasari Sudeep <sudeep.y@fireflink.com>
2024-01-18 10:31:06 +0530

    Added role for user login response and changed login request password key

--- src/main/java/com/Ychits/util/jwt/LoginResponse.java
+++ src/main/java/com/Ychits/util/jwt/LoginResponse.java
@@ -5,10 +5,12 @@ import lombok.Data;
 @Data
 public class LoginResponse {
     private String id;
+    private String role;
     private String token;
 
-    public LoginResponse(String id, String token) {
+    public LoginResponse(String id,String role, String token) {
         this.id=id;
+        this.role=role;
         this.token=token;
     }
     public LoginResponse() {
